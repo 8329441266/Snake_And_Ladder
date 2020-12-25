e54cc438984b15afae4583237723ef1cd7ae90aa
8329441266 <sadashivingale2810@gmail.com>
2020-12-25 10:31:39 -0800

    total dice counting

--- Utility.js
+++ Utility.js
@@ -7,10 +7,11 @@ class Utility{
     current_position = 0;
 
     rollDice = () => {
+        let dice_count = 0;
         this.current_position = this.START_POSITION;       
         while(this.current_position<this.END_POSITION){
         var dice = Math.floor( Math.random() * 6 ) + 1 ;
-        console.log('Random check dice : '+dice);
+        console.log('Random check dice :'+dice);
         let check_option = Math.floor(Math.random()*3)+1;
 
             switch(check_option){
@@ -33,8 +34,10 @@ class Utility{
                             this.current_position = this.current_position;
                             break;
                 }  
+                dice_count++;
             }
                 console.log('Current position :${this.current_position}');     
+                console.log('Dice count is :'+dice_count);
     }
 }
 module.exports=new Utility();
