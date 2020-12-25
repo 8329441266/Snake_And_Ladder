65f6bb1d6475d1e663341d940b4f0b251a4e1900
8329441266 <sadashivingale2810@gmail.com>
2020-12-25 13:29:40 -0800

    Winner declare

--- Utility.js
+++ Utility.js
@@ -1,40 +1,73 @@
 class Utility{
-    START_POSITION = 0;
-    END_POSITION = 100;
-    SNAKE = 1;
-    LADDER = 2;
-    NO_PLAY = 0;
-    current_position = 0;
 
-    rollDice = () => {
-        this.current_position = this.START_POSITION;       
-        while(this.current_position<this.END_POSITION){
-        var dice = Math.floor( Math.random() * 6 ) + 1 ;
-        console.log('Random check dice : '+dice);
-        let check_option = Math.floor(Math.random()*3)+1;
+     START_POSITION = 0;
+     END_POSITION = 100;
+     SNAKE=1;
+     LADDER=2;
+     NO_PLAY=3;
+     current_position;  
+     dice_count;
+     
+      rollDice = (current_position) => {
 
-            switch(check_option){
-                case this.SNAKE:
-                    console.log('SNAKE');
-                    if(this.current_position - dice >= this.STARTING_POSITION){
-                        this.current_position = this.current_position - dice;   
-                    }
-                     break;
+        if(current_position<this.END_POSITION)
+        {
+          let dice=Math.floor(Math.random()*6)+1;
+          console.log(`dice | ${dice}`);
+          let check_option=Math.floor(Math.random()*3)+1;
+         switch(check_option)
+           {
+             case this.SNAKE:
+                  console.log('snake |');
+                 if(current_position-dice>=this.START_POSITION)
+                 {
+                  current_position=current_position-dice;
+                 }
+                 break;
+             case this.LADDER:
+                 console.log('ladder |');
+                 if(current_position+dice<=this.END_POSITION)
+                 {
+                 current_position=current_position+dice;
+                 } 
+                break;
+             case this.NO_PLAY:
+                 console.log('no play |');
+                 current_position=current_position;
+                 break;
+           }
+           this.dice_count++;
+       }
+         console.log(`current position is ${current_position}`);
+         return current_position;
+      }
+  
+      playerstwo = () => {
 
-                    case this.LADDER :
-                        console.log(' LADDER ');
-                        if(this.current_position + dice <= this.ENDING_POSITION){
-                            this.current_position = this.current_position + dice;
-                        }
-                        break;
-
-                    default:
-                        console.log('NO PLAY');
-                            this.current_position = this.current_position;
-                            break;
-                }  
-            }
-                console.log('Current position :${this.current_position}');     
+          let Player1Position = this.START_POSITION
+          let Player2Position = this.START_POSITION
+          this.dice_Count = this.START_POSITION
+         while( Player1Position < this.END_POSITION && Player2Position <  this.END_POSITION )
+          {
+             console.log('Player 1:   ');
+             Player1Position = this.rollDice(Player1Position);
+             console.log("..............................................................");
+             if(Player1Position == this.END_POSITION)
+             {
+               console.log("Player 1 Won This Game ");
+                 break;
+             }
+  
+             console.log('Player 2:  ');
+             Player2Position = this.rollDice(Player2Position);
+             console.log("............................................................");
+             if(Player2Position == this.END_POSITION)
+             {
+              console.log("Player 2 Won This Game ");
+                break;
+             }            
+         }
     }
-}
-module.exports=new Utility();
+   
+  }
+  module.exports = new Utility();
